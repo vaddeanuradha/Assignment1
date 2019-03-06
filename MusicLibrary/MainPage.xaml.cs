b03bd065b184754c5bfb8ae37c346840eee27b2f
Anuradha Vadde <vaddeanuradha@gmail.com>
2019-03-06 12:20:12 -0800

    Commit

---
+++ MusicLibrary/MainPage.xaml.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Media.Core;
+using Windows.Storage;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Windows.Media.Playback;
+using Windows.Media;
+using Windows.System.Display;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace MusicLibrary
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        private ObservableCollection<MusicLib> MusicList = new ObservableCollection<MusicLib>();
+
+        public DisplayRequest appDisplayRequest { get; private set; }
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+       
+        protected override async void OnNavigatedTo(NavigationEventArgs e)
+        {
+            StorageFolder musicLib = KnownFolders.MusicLibrary;
+            
+            var files = await musicLib.GetFilesAsync();
+            foreach (var file in files)
+            {
+                var musicProperties = await file.Properties.GetMusicPropertiesAsync();
+                var artist = musicProperties.Artist;
+                if (artist == "")
+                    artist = "Artista desconocido";
+                var album = musicProperties.Album;
+                if (album == "")
+                    album = "Unkown";
+                MusicList.Add(new MusicLib { FileName = file.DisplayName, Artist = artist, Album = album, MusicFile = file });
+            }
+
+           
+
+        }
+       
+        private void ListView_ItemClick(object sender, ItemClickEventArgs e)
+        {
+            //mediaPlayer.Source = new Uri(listview.SelectedValuePath);
+
+            var clickedItem = (MusicLib)e.ClickedItem;
+            mediaPlayer.Source = MediaSource.CreateFromStorageFile(clickedItem.MusicFile);
+            mediaPlayer.MediaPlayer.Play();
+            //mediaPlayer.MediaPlayer.Pause();
+            
+
+
+        }
+
+
+    }
+}
---
+++ MusicLibrary/MusicLib.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Storage;
+
+namespace MusicLibrary
+{
+    public class MusicLib
+    {
+        public string FileName { get; set; }
+        public string Artist { get; set; }
+        public string Album { get; set; }
+        public StorageFile MusicFile { get; set; }
+
+    }
+}
